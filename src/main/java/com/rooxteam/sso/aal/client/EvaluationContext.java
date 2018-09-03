b96e9624800fedb2c1f26f83d36eb885b3aaf939
drudenko <dimarudenk@gmail.com>
2018-09-03 17:16:20 +0300

    no-task: fix construct, refactory EvaluationContext

--- src/main/java/com/rooxteam/sso/aal/client/EvaluationContext.java
+++ src/main/java/com/rooxteam/sso/aal/client/EvaluationContext.java
@@ -1,19 +1,25 @@
 package com.rooxteam.sso.aal.client;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.Map;
+
+import static java.util.Optional.ofNullable;
 
 /**
  * hold all context for policy evaluation
  */
 @Getter
+@Setter
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EvaluationContext implements Serializable {
     public static final String WEB_AGENT_SERVICE_NAME = "iPlanetAMWebAgentService";
@@ -26,18 +32,19 @@ public class EvaluationContext implements Serializable {
     private String realm;
 
     public EvaluationContext(String realm, String resourceName, String actionName, Map envParams) {
-        this.realm = realm;
-        this.resourceName = resourceName;
-        this.actionName = actionName;
-        this.envParams = envParams;
-        this.extraParams = envParams;
+        this(null, actionName, resourceName, envParams, null, realm);
     }
 
     public EvaluationContext(String realm, String resourceName, String actionName, Map envParams, Map extraParams) {
-        this.realm = realm;
-        this.resourceName = resourceName;
+        this(null, actionName, resourceName, envParams, extraParams, realm);
+    }
+
+    public EvaluationContext(String serviceName, String actionName, String resourceName, Map envParams, Map extraParams, String realm) {
+        this.serviceName = ofNullable(serviceName).orElse(WEB_AGENT_SERVICE_NAME);
         this.actionName = actionName;
+        this.resourceName = resourceName;
         this.envParams = envParams;
-        this.extraParams = extraParams;
+        this.extraParams = ofNullable(extraParams).orElse(envParams);
+        this.realm = realm;
     }
 }
