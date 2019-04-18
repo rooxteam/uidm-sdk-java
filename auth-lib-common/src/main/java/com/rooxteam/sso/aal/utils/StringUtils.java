4f10910be53ca41be595ddc688738819f0746453
Konstantin Korsakov <kkorsakov@outlook.com>
2019-04-18 19:58:54 +0300

    don't depend on apache commons lang

---
+++ auth-lib-common/src/main/java/com/rooxteam/sso/aal/utils/StringUtils.java
@@ -0,0 +1,14 @@
+package com.rooxteam.sso.aal.utils;
+
+/**
+ * We are not going to depend on whole commons-lang for this utils
+ */
+public final class StringUtils {
+
+    private StringUtils() {
+    }
+
+    public static boolean isEmpty(String string) {
+        return string == null || string.length() == 0;
+    }
+}
