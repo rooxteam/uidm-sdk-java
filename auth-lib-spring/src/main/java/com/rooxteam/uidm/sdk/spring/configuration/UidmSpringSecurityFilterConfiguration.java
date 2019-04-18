1be6ed2bea378216e64425e4ae651627ebd7fc4a
Konstantin Korsakov <kkorsakov@outlook.com>
2019-04-18 17:42:55 +0300

    PD-11543 add filter for sticky-sessioned requests to UIDM

--- auth-lib-spring/src/main/java/com/rooxteam/uidm/sdk/spring/configuration/UidmSpringSecurityFilterConfiguration.java
+++ auth-lib-spring/src/main/java/com/rooxteam/uidm/sdk/spring/configuration/UidmSpringSecurityFilterConfiguration.java
@@ -1,12 +1,16 @@
 package com.rooxteam.uidm.sdk.spring.configuration;
 
+import com.rooxteam.sso.aal.client.cookies.CookieStoreFactory;
+import com.rooxteam.sso.aal.client.cookies.RequestCookieStoreFilter;
 import com.rooxteam.uidm.sdk.spring.authentication.SsoAuthorizationClient;
 import com.rooxteam.uidm.sdk.spring.authentication.UidmUserPreAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.Filter;
 
 @Configuration
 @Import(UidmSdkConfiguration.class)
@@ -14,8 +18,16 @@ public class UidmSpringSecurityFilterConfiguration {
 
 
     @Bean
-    public GenericFilterBean uidmUserPreAuthenticationFilter(SsoAuthorizationClient ssoAuthorizationClient) throws Exception {
+    public GenericFilterBean uidmUserPreAuthenticationFilter(SsoAuthorizationClient ssoAuthorizationClient) {
         return new UidmUserPreAuthenticationFilter(ssoAuthorizationClient);
     }
+
+
+    @Bean
+    @Order(0)
+    public Filter requestContextFilter() {
+        return new RequestCookieStoreFilter(new CookieStoreFactory());
+    }
+
 
 }
